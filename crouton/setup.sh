64ffd9a874b605a463b063906669aa910f86adab
Alexander Ivanov <oshli.a.er@gmail.com>
2017-08-14 08:42:16 -0700

    Update setup.sh

--- crouton/setup.sh
+++ crouton/setup.sh
@@ -1,3 +1,17 @@
-locale-gen en_US.UTF-8
-locale-gen ru_RU.UTF-8
+if locale -a | grep -i ^en_us.utf ; then
+  # locale installed...
+  echo "en_us.utf is installed"
+else
+  # locale not installed...
+  locale-gen en_US.utf8
+fi
+
+if locale -a | grep -i ^ru_ru.utf ; then
+  # locale installed...
+  echo "ru_ru.utf is installed"
+else
+  # locale not installed...
+  locale-gen ru_RU.utf8
+fi
+
 update-locale
